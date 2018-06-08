88a9645413e77d56c88efeef44afce9b8864c590
ziginsider <ziginsider@gmail.com>
2018-06-08 17:09:47 +0300

    add AddNoteActivity

---
+++ app/src/main/java/io/github/ziginsider/testproject/addnote/AddNoteActivity.kt
@@ -0,0 +1,13 @@
+package io.github.ziginsider.testproject.addnote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.ziginsider.testproject.R
+
+class AddNoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addnote)
+    }
+}
