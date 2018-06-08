3c78f0a57c885c005934738aefbf0dcc49ad40f6
ziginsider <ziginsider@gmail.com>
2018-06-08 17:13:13 +0300

    add NoteDetailActivity

---
+++ app/src/main/java/io/github/ziginsider/testproject/detailnote/NoteDetailActivity.kt
@@ -0,0 +1,13 @@
+package io.github.ziginsider.testproject.detailnote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.ziginsider.testproject.R
+
+class NoteDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
