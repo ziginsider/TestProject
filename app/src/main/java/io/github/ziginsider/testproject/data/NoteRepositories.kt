08c8a4ba4e793f571bed5ec7f9e8d9a491a74ca0
ziginsider <ziginsider@gmail.com>
2018-06-08 18:28:54 +0300

    add NoteRepositories class

---
+++ app/src/main/java/io/github/ziginsider/testproject/data/NoteRepositories.kt
@@ -0,0 +1,17 @@
+package io.github.ziginsider.testproject.data
+
+class NoteRepositories {
+
+    companion object {
+        var repository: NotesRepository? = null
+
+        @Synchronized
+        fun getInMemoryRepoInstance(notesServiceApi: NotesServiceApi): NotesRepository {
+            //checkNotNull(notesServiceApi)
+            if (null == repository) {
+                repository = InMemoryNotesRepository(notesServiceApi)
+            }
+            return repository
+        }
+    }
+}
