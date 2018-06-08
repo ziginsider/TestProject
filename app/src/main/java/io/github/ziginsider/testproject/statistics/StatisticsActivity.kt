8f25453f11e8757636fcc67fde1880dc47193255
ziginsider <ziginsider@gmail.com>
2018-06-08 17:58:41 +0300

    add StatisticsActivity

---
+++ app/src/main/java/io/github/ziginsider/testproject/statistics/StatisticsActivity.kt
@@ -0,0 +1,13 @@
+package io.github.ziginsider.testproject.statistics
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.ziginsider.testproject.R
+
+class StatisticsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
+    }
+}
