0d67b10c42caf16b40e278ed8e7ddb092e8f9f18
ziginsider <ziginsider@gmail.com>
2018-06-10 15:00:27 +0300

    add NotesRepository interface

---
+++ app/src/main/java/io/github/ziginsider/testproject/data/NotesRepository.kt
@@ -0,0 +1,22 @@
+package io.github.ziginsider.testproject.data
+
+interface NotesRepository {
+
+    interface LoadNotesCallback {
+
+        fun onNotesLoaded(notes: List<Note>)
+    }
+
+    interface GetNoteCallback {
+
+        fun onNoteLoaded(note: Note)
+    }
+
+    fun getNotes(callback: LoadNotesCallback)
+
+    fun getNote(noteId: String, callback: GetNoteCallback)
+
+    fun saveNote(note: Note)
+
+    fun refreshData()
+}
